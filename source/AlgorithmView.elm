51ecb34467167f3ba552180b50bcbe6151229e81
Ben Anhalt <anhalt@ku.edu>
2017-05-24 17:57:20 -0500

    add algorithms view and make cards expandable.

--- source/AlgorithmView.elm
+++ source/AlgorithmView.elm
@@ -10,6 +10,8 @@ import Decoder
 import Html exposing (Html)
 import Html.Attributes as Attributes
 import Material
+import Material.Elevation as Elevation
+import Material.Button as Button
 import Material.Helpers exposing (lift)
 import Material.Options as Options
 import Material.Card as Card
@@ -25,6 +27,8 @@ type alias Index =
 type alias Model =
     { definition : D.Algorithm
     , parameters : Array.Array ParameterView.Model
+    , truncateDesc : Bool
+    , expandParams : Bool
     , mdl : Material.Model
     }
 
@@ -48,6 +52,8 @@ init : D.Algorithm -> Model
 init def =
     { definition = def
     , parameters = Array.fromList <| List.map ParameterView.init def.parameters
+    , truncateDesc = True
+    , expandParams = False
     , mdl = Material.model
     }
 
@@ -55,11 +61,19 @@ init def =
 type Msg
     = Mdl (Material.Msg Msg)
     | ParameterMsg Int ParameterView.Msg
+    | ToggleDesc
+    | ToggleParams
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        ToggleDesc ->
+            ( { model | truncateDesc = not model.truncateDesc }, Cmd.none )
+
+        ToggleParams ->
+            ( { model | expandParams = not model.expandParams }, Cmd.none )
+
         Mdl msg_ ->
             Material.update Mdl msg_ model
 
@@ -80,19 +94,55 @@ update msg model =
                 model
 
 
+descriptionView : Model -> Html Msg
+descriptionView model =
+    let
+        desc =
+            model.definition.description
+    in
+        Options.styled Html.p
+            [ Options.onClick ToggleDesc ]
+            [ if model.truncateDesc && String.length desc > 100 then
+                Html.text <| (String.left 100 desc) ++ "..."
+              else
+                Html.text desc
+            ]
+
+
 parameterView : Index -> Int -> ParameterView.Model -> Html Msg
 parameterView index i model =
     Html.map (ParameterMsg i) <| ParameterView.view (i :: index) <| model
 
 
+parametersView : Index -> Model -> List (Html Msg)
+parametersView index model =
+    if model.expandParams then
+        [ List.indexedMap (parameterView index) (Array.toList model.parameters)
+            |> Html.ul [ Attributes.style [ ( "padding", "0" ), ( "list-style", "none" ) ] ]
+        ]
+    else
+        []
+
+
 view : Index -> Model -> Html Msg
 view index model =
-    Card.view [ Options.css "width" "400px" ]
+    Card.view [  Elevation.e2 ]
         [ Card.title [] [ Card.head [] [ Html.text model.definition.name ] ]
-        , Card.text [] [ Html.text model.definition.description ]
-        , Card.actions []
-            [ Html.ul [ Attributes.style [ ( "padding", "0" ), ( "list-style", "none" ) ] ] <|
-                List.indexedMap (parameterView index) (Array.toList model.parameters)
+        , Card.text []
+            (descriptionView model :: parametersView index model)
+        , Card.actions [ Card.border ]
+            [ Button.render Mdl
+                (-1 :: index)
+                model.mdl
+                [ Options.onClick ToggleParams
+                ]
+                [ Html.text
+                    (if model.expandParams then
+                        "Hide Parameters"
+                     else
+                        "Show Parameters"
+                    )
+                ]
             ]
         ]
 
---
+++ source/AlgorithmsView.elm
@@ -0,0 +1,79 @@
+module AlgorithmsView exposing (..)
+
+import Array
+import Html exposing (Html)
+import Material
+import Material.Scheme
+import Material.Grid as Grid exposing (Cell, Device(..), grid, cell)
+import Material.Helpers exposing (lift)
+import AlgorithmView
+import AlgorithmDefinition as D
+
+
+type alias Index =
+    List Int
+
+
+type alias Model =
+    { algorithms : Array.Array AlgorithmView.Model
+    , mdl : Material.Model
+    }
+
+
+type Msg
+    = Mdl (Material.Msg Msg)
+    | AlgorithmMsg Int AlgorithmView.Msg
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Mdl msg_ ->
+            Material.update Mdl msg_ model
+
+        AlgorithmMsg i msg_ ->
+            lift
+                (\m ->
+                    case Array.get i m.algorithms of
+                        Just m ->
+                            m
+
+                        Nothing ->
+                            Debug.crash "what?"
+                )
+                (\m x -> { m | algorithms = Array.set i x m.algorithms })
+                (AlgorithmMsg i)
+                AlgorithmView.update
+                msg_
+                model
+
+
+viewAlgorithm : Index -> Int -> AlgorithmView.Model -> Cell Msg
+viewAlgorithm index i model =
+    cell [ Grid.size All 2 ] [ Html.map (AlgorithmMsg i) <| AlgorithmView.view (i :: index) <| model ]
+
+
+view : Index -> Model -> Html Msg
+view index model =
+    grid [] <|
+        List.indexedMap (viewAlgorithm index) (Array.toList model.algorithms)
+
+
+init : Model
+init =
+    { algorithms = Array.fromList <| List.map AlgorithmView.init D.algorithms
+    , mdl = Material.model
+    }
+
+
+main =
+    Html.program
+        { init = ( init, Material.init Mdl )
+        , view = view [] >> Material.Scheme.top
+        , update = update
+        , subscriptions =
+            \model ->
+                Sub.batch
+                    [ Material.subscriptions Mdl model
+                    ]
+        }
