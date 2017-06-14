b028cf07fef9681aa0b0d869530edc02c9b50104
Ben Anhalt <anhalt@ku.edu>
2017-06-14 16:12:36 -0500

    add maps to occurrence set tab.

--- source/OccurrenceSetsView.elm
+++ source/OccurrenceSetsView.elm
@@ -1,6 +1,13 @@
 module OccurrenceSetsView exposing (..)
 
-import Decoder exposing (AtomObjectRecord)
+import Decoder
+    exposing
+        ( AtomObjectRecord
+        , decodeOccurrenceSet
+        , OccurrenceSetRecord
+        , OccurrenceSet(..)
+        , SingleLayerMap(..)
+        )
 import OccurrenceSetChooser
 import Material
 import Material.Options as Options
@@ -9,12 +16,16 @@ import Material.List as L
 import Material.Helpers as Helpers
 import Html exposing (Html)
 import Html.Events
+import Http
 import Helpers exposing (Index)
+import MapCard
 
 
 type alias Model =
     { occurrenceSets : List AtomObjectRecord
     , chooser : OccurrenceSetChooser.Model
+    , mappedSet : Maybe OccurrenceSetRecord
+    , mapCard : MapCard.Model
     , mdl : Material.Model
     }
 
@@ -22,6 +33,9 @@ type alias Model =
 type Msg
     = ChooserMsg OccurrenceSetChooser.Msg
     | Remove Int
+    | SetMapped (Maybe OccurrenceSetRecord)
+    | MapOccurrences Int
+    | MapCardMsg MapCard.Msg
     | Mdl (Material.Msg Msg)
 
 
@@ -41,6 +55,27 @@ update msg model =
             , Cmd.none
             )
 
+        MapOccurrences id ->
+            ( model, getMetadata id )
+
+        SetMapped o ->
+            Helpers.lift
+                .mapCard
+                (\m x -> { m | mapCard = x })
+                MapCardMsg
+                MapCard.update
+                (updateMap o)
+                ({ model | mappedSet = o })
+
+        MapCardMsg msg_ ->
+            Helpers.lift
+                .mapCard
+                (\m x -> { m | mapCard = x })
+                MapCardMsg
+                MapCard.update
+                msg_
+                model
+
         ChooserMsg msg_ ->
             Helpers.lift
                 .chooser
@@ -57,21 +92,70 @@ update msg model =
                 )
 
 
+updateMap : Maybe OccurrenceSetRecord -> MapCard.Msg
+updateMap =
+    Maybe.andThen (\{ map } -> map)
+        >> Maybe.map
+            (\(SingleLayerMap { endpoint, mapName, layerName }) ->
+                { endPoint = endpoint, mapName = mapName, layers = [ layerName ] }
+            )
+        >> MapCard.SetMap
+
+
+getMetadata : Int -> Cmd Msg
+getMetadata id =
+    Http.request
+        { method = "GET"
+        , headers = [ Http.header "Accept" "application/json" ]
+        , url = "http://notyeti-191.lifemapper.org/api/v2/occurrence/" ++ (toString id)
+        , body = Http.emptyBody
+        , expect = Http.expectJson decodeOccurrenceSet
+        , timeout = Nothing
+        , withCredentials = False
+        }
+        |> Http.send gotMetadata
+
+
+gotMetadata : Result Http.Error OccurrenceSet -> Msg
+gotMetadata result =
+    case result of
+        Ok (OccurrenceSet o) ->
+            SetMapped (Just o)
+
+        Err err ->
+            SetMapped Nothing
+                |> Debug.log (toString err)
+
+
 init : Model
 init =
     { occurrenceSets = []
+    , mappedSet = Nothing
     , chooser = OccurrenceSetChooser.init
+    , mapCard = MapCard.init "leaflet-map-occurrence-sets"
     , mdl = Material.model
     }
 
 
 occurrenceSetLI : Model -> Index -> Int -> AtomObjectRecord -> Html Msg
 occurrenceSetLI model index i o =
-    L.li []
-        [ L.content [] [ Html.text o.name ]
-        , L.content2 []
-            [ L.icon "delete" [ Options.attribute <| Html.Events.onClick (Remove i) ] ]
-        ]
+    let
+        iconName =
+            if Just o.id == Maybe.map .id model.mappedSet then
+                "visibility"
+            else
+                "visibility_off"
+
+        icon =
+            L.icon iconName [ Options.attribute <| Html.Events.onClick (MapOccurrences o.id) ]
+    in
+        L.li []
+            [ L.content [] [ Html.text o.name ]
+            , L.content2 []
+                [ L.icon "delete" [ Options.attribute <| Html.Events.onClick (Remove i) ]
+                , icon
+                ]
+            ]
 
 
 occurrenceSetList : Index -> Model -> Html Msg
@@ -89,5 +173,5 @@ view : Index -> Model -> Html Msg
 view index model =
     Options.div [ Options.css "display" "flex" ]
         [ occurrenceSetList index model
-          -- , mapCard index model
+        , MapCard.view index "Map" model.mapCard |> Html.map MapCardMsg
         ]
