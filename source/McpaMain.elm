6d5c1517274380eb7fb0df60f8dcc1f352e980e9
Ben Anhalt <anhalt@ku.edu>
2017-10-11 12:48:34 -0500

    separate treezipper code.

--- source/McpaMain.elm
+++ source/McpaMain.elm
@@ -8,73 +8,11 @@ import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import ExampleTree
 import DecodeTree exposing (Tree(..), TreeData)
+import TreeZipper exposing (..)
 import Time exposing (Time)
 import Animation as A exposing (Animation)
 import AnimationFrame
 import Ease
-
-
-type Context
-    = Top
-    | Left TreeData Context Tree
-    | Right TreeData Tree Context
-
-
-type alias TreeZipper =
-    ( Tree, Context )
-
-
-top : Tree -> TreeZipper
-top tree =
-    ( tree, Top )
-
-
-left : TreeZipper -> TreeZipper
-left ( tree, context ) =
-    case tree of
-        Node data left right ->
-            ( left, Left data context right )
-
-        _ ->
-            ( tree, context )
-
-
-right : TreeZipper -> TreeZipper
-right ( tree, context ) =
-    case tree of
-        Node data left right ->
-            ( right, Right data left context )
-
-        _ ->
-            ( tree, context )
-
-
-up : TreeZipper -> TreeZipper
-up ( tree, context ) =
-    case context of
-        Top ->
-            ( tree, context )
-
-        Left data upContext right ->
-            ( Node data tree right, upContext )
-
-        Right data left upContext ->
-            ( Node data left tree, upContext )
-
-
-getTree : TreeZipper -> Tree
-getTree ( tree, context ) =
-    tree
-
-
-getData : TreeZipper -> TreeData
-getData zipper =
-    case getTree zipper of
-        Leaf data ->
-            data
-
-        Node data _ _ ->
-            data
 
 
 type AnimationDirection
@@ -119,14 +57,14 @@ updateAnimation msg ({ zipper, animationState } as model) =
         Static ->
             case msg of
                 KeyUp "ArrowDown" ->
-                    case zipper of
-                        ( _, Left _ _ _ ) ->
+                    case getPosition zipper of
+                        LeftBranch ->
                             ( { model | animationState = Start AnimateUpLeft }, Cmd.none )
 
-                        ( _, Right _ _ _ ) ->
+                        RightBranch ->
                             ( { model | animationState = Start AnimateUpRight }, Cmd.none )
 
-                        _ ->
+                        Root ->
                             ( model, Cmd.none )
 
                 KeyUp "ArrowLeft" ->
---
+++ source/TreeZipper.elm
@@ -0,0 +1,85 @@
+module TreeZipper exposing (Position(..), TreeZipper, top, left, right, up, getTree, getData, getPosition)
+
+import DecodeTree exposing (Tree(..), TreeData)
+
+
+type Position
+    = Root
+    | LeftBranch
+    | RightBranch
+
+
+type Context
+    = Top
+    | Left TreeData Context Tree
+    | Right TreeData Tree Context
+
+
+type TreeZipper
+    = TreeZipper Tree Context
+
+
+top : Tree -> TreeZipper
+top tree =
+    TreeZipper tree Top
+
+
+left : TreeZipper -> TreeZipper
+left ((TreeZipper tree context) as zipper) =
+    case tree of
+        Node data left right ->
+            TreeZipper left (Left data context right)
+
+        _ ->
+            zipper
+
+
+right : TreeZipper -> TreeZipper
+right ((TreeZipper tree context) as zipper) =
+    case tree of
+        Node data left right ->
+            TreeZipper right (Right data left context)
+
+        _ ->
+            zipper
+
+
+up : TreeZipper -> TreeZipper
+up ((TreeZipper tree context) as zipper) =
+    case context of
+        Top ->
+            zipper
+
+        Left data upContext right ->
+            TreeZipper (Node data tree right) upContext
+
+        Right data left upContext ->
+            TreeZipper (Node data left tree) upContext
+
+
+getTree : TreeZipper -> Tree
+getTree (TreeZipper tree context) =
+    tree
+
+
+getData : TreeZipper -> TreeData
+getData zipper =
+    case getTree zipper of
+        Leaf data ->
+            data
+
+        Node data _ _ ->
+            data
+
+
+getPosition : TreeZipper -> Position
+getPosition (TreeZipper _ context) =
+    case context of
+        Top ->
+            Root
+
+        Left _ _ _ ->
+            LeftBranch
+
+        Right _ _ _ ->
+            RightBranch
