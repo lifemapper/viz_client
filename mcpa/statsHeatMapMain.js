d4f203cd51085af864c89945cd5e5f91a4b8faa9
Ben Anhalt <anhalt@ku.edu>
2019-04-01 15:09:12 -0500

    speed up stats heat map page.

--- mcpa/statsHeatMapMain.js
+++ mcpa/statsHeatMapMain.js
@@ -23,10 +23,110 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 
 "use strict";
 
-var app = Elm.StatsHeatMap.fullscreen();
 
 var maps = {};
 var mapLayers = {};
+
+function configureMap(element) {
+    var map = maps[element._leaflet_id];
+    if (map == null) return;
+    console.log("updating leaflet id", element._leaflet_id);
+
+    var layers = mapLayers[element._leaflet_id];
+    // if (layers != null) {
+    //     layers.forEach(function(layer) {  map.removeLayer(layer); });
+    // }
+
+    var variable = element.dataset["mapSelectedVar"];
+
+    console.log("adding layer");
+
+    if (layers == null || layers.length === 0) {
+        mapLayers[element._leaflet_id] = [
+            L.geoJSON(sitesObserved, {
+                style: style(variable),
+                filter: function(feature) {
+                    return Object.values(feature.properties).some(function(value) { return value !== 0.0; });
+                }
+            }).addTo(map)
+        ];
+    } else {
+        layers[0].setStyle(style(variable));
+    }
+}
+
+function style(variable) {
+    const values = sitesObserved.features.map(function(feature) { return feature.properties[variable]; });
+    const max = Math.max.apply(Math, values);
+    const min = Math.min.apply(Math, values);
+    return function(feature) {
+        const value = (feature.properties[variable] - min) / (max - min);
+        const style = {
+            fillOpacity: value,
+            stroke: false,
+            fill: true,
+            fillColor: "red"
+        };
+        return style;
+    };
+}
+
+
+var bbox = turf.bbox(sitesObserved);
+
+var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(m) {
+        m.addedNodes.forEach(function(n) {
+            if (n.getElementsByClassName == null) return;
+
+            var elements = n.getElementsByClassName("leaflet-map");
+            Array.prototype.forEach.call(elements, function(element) {
+                var map = L.map(element).fitBounds([
+                    [bbox[1], bbox[0]], [bbox[3], bbox[2]]
+                ]);
+                L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: "© OpenStreetMap",
+                    minZoom: 2,
+                    maxZoom: 12,
+                    worldCopyJump: true
+                }).addTo(map);
+
+                maps[element._leaflet_id] = map;
+                console.log("added leaflet id", element._leaflet_id);
+                configureMap(element);
+            });
+        });
+
+        m.removedNodes.forEach(function(n) {
+            if (n.getElementsByClassName == null) return;
+
+            var elements = n.getElementsByClassName("leaflet-map");
+            Array.prototype.forEach.call(elements, function(element) {
+                if (element._leaflet_id != null) {
+                    console.log("removing map with leaflet id", element._leaflet_id);
+                    maps[element._leaflet_id].remove();
+                    maps[element._leaflet_id] = null;
+                    mapLayers[element._leaflet_id] = null;
+                }
+            });
+        });
+
+        if (m.type == "attributes") {
+            configureMap(m.target);
+        }
+    });
+});
+
+observer.observe(document.body, {
+    subtree: true,
+    childList: true,
+    attributes: true,
+    attributeFilter: ["data-map-selected-var"],
+    attributeOldValue: true
+});
+
+
+var app = Elm.StatsHeatMap.fullscreen();
 
 app.ports.requestStats.subscribe(function() {
     const statRanges = sitesObserved.features.reduce(function(ranges, feature) {
@@ -65,136 +165,4 @@ document.onmousemove = document.onmouseup = document.onmousedown = function(even
         ctrlKey: event.ctrlKey
     });
 };
-
-function configureMap(element) {
-    var map = maps[element._leaflet_id];
-    if (map == null) return;
-    console.log("updating leaflet id", element._leaflet_id);
-
-    var layers = mapLayers[element._leaflet_id];
-    // if (layers != null) {
-    //     layers.forEach(function(layer) {  map.removeLayer(layer); });
-    // }
-
-    var variable = element.dataset["mapSelectedVar"];
-
-    console.log("adding layer");
-
-    if (layers == null || layers.length === 0) {
-        mapLayers[element._leaflet_id] = [
-            L.geoJSON(sitesObserved, {style: style(variable)}).addTo(map)
-        ];
-    } else {
-        layers[0].setStyle(style(variable));
-    }
-}
-
-function style(variable) {
-    const values = sitesObserved.features.map(function(feature) { return feature.properties[variable]; });
-    const max = Math.max.apply(Math, values);
-    const min = Math.min.apply(Math, values);
-    return function(feature) {
-        const value = (feature.properties[variable] - min) / (max - min);
-        const style = {
-            fillOpacity: value,
-            stroke: false,
-            fill: true,
-            fillColor: "red"
-        };
-        return style;
-    };
-}
-
-// var centers = turf.featureCollection(
-//     turf.featureReduce(ancPam, function(centers, feature) {
-//         return centers.concat(
-//             turf.point([feature.properties.centerX, feature.properties.centerY], feature.properties)
-//         );
-//     }, [])
-// );
-
-var bbox = turf.bbox(sitesObserved);
-
-var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(m) {
-        m.addedNodes.forEach(function(n) {
-            if (n.getElementsByClassName == null) return;
-
-            var elements = n.getElementsByClassName("leaflet-map");
-            Array.prototype.forEach.call(elements, function(element) {
-                var map = L.map(element).fitBounds([
-                    [bbox[1], bbox[0]], [bbox[3], bbox[2]]
-                ]);
-                L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                    attribution: "© OpenStreetMap",
-                    minZoom: 2,
-                    maxZoom: 12,
-                    worldCopyJump: true
-                }).addTo(map);
-
-                var editableLayers = new L.FeatureGroup();
-                map.addLayer(editableLayers);
-
-                var drawControl = new L.Control.Draw({
-                    draw: {
-                        polyline: false,
-                        marker: false,
-                        circle: false,
-                        circlemarker: false
-                    }
-                });
-                map.addControl(drawControl);
-
-                map.on(L.Draw.Event.CREATED, function(e) {
-                    editableLayers.addLayer(e.layer);
-                    const selection = editableLayers.toGeoJSON().features[0];
-                    app.ports.sitesSelected.send(
-                        turf.featureReduce(
-                            sitesObserved,
-                            function(sites, feature) {
-                                return turf.booleanWithin(feature, selection) ?
-                                    sites.concat(feature.id) :
-                                    sites;
-                            }, [])
-                    );
-                    editableLayers.clearLayers();
-                });
-
-                map.on(L.Draw.Event.DRAWSTART, function(e) {
-                    editableLayers.clearLayers();
-                });
-
-                maps[element._leaflet_id] = map;
-                console.log("added leaflet id", element._leaflet_id);
-                configureMap(element);
-            });
-        });
-
-        m.removedNodes.forEach(function(n) {
-            if (n.getElementsByClassName == null) return;
-
-            var elements = n.getElementsByClassName("leaflet-map");
-            Array.prototype.forEach.call(elements, function(element) {
-                if (element._leaflet_id != null) {
-                    console.log("removing map with leaflet id", element._leaflet_id);
-                    maps[element._leaflet_id].remove();
-                    maps[element._leaflet_id] = null;
-                    mapLayers[element._leaflet_id] = null;
-                }
-            });
-        });
-
-        if (m.type == "attributes") {
-            configureMap(m.target);
-        }
-    });
-});
-
-observer.observe(document.body, {
-    subtree: true,
-    childList: true,
-    attributes: true,
-    attributeFilter: ["data-map-selected-var"],
-    attributeOldValue: true
-});
 
